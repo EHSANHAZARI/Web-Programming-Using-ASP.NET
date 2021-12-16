d84733b7672a79898a7dd5ffd50a8727b552c0d1
EHSANHAZARI <57685610+EHSANHAZARI@users.noreply.github.com>
2021-12-15 22:55:27 -0500

    Add files via upload

---
+++ BaseController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace F2021A6MH.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly Manager manager;
+
+        public BaseController()
+        {
+            manager = new Manager();
+        }
+
+        protected SelectList GetGenreSelectList() => new SelectList(manager.GenreGetAll(), "Name", "Name");
+    }
+}
