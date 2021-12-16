80665efa7e58915c777052371e6e2a2025ea73a8
EHSANHAZARI <57685610+EHSANHAZARI@users.noreply.github.com>
2021-12-15 23:03:59 -0500

    Add files via upload

---
+++ EntityModels/ArtistMediaItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace F2021A6MH.EntityModels
+{
+    public class ArtistMediaItem
+    {
+        public ArtistMediaItem()
+        {
+            Timestamp = DateTime.Now;
+
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1);
+            }
+            StringId = string.Format("{0:x}", i - DateTime.Now.Ticks);
+        }
+
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string StringId { get; set; }
+
+        [Required, StringLength(100)]
+        public string Caption { get; set; }
+
+        public byte[] Content { get; set; }
+
+        [StringLength(200)]
+        public string ContentType { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        [Required]
+        public Artist Artist { get; set; }
+    }
+}
